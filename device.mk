5b147b0c7085905ed0679e5958a2b1a070dbea40
Elite <elitebitvavo@gmail.com>
2026-02-15 20:58:19 +0400

    temp test sign

--- device.mk
+++ device.mk
@@ -25,3 +25,6 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+-include vendor/lineage-priv/keys/keys.mk
+include vendor/lineage-priv/keys/keys.mk
